e2722e5763ff2d7ffb899d1cdf94f2fb80c17f54
Karin Hendrikse <30577427+khendrikse@users.noreply.github.com>
2022-08-03 17:34:31 +0200

    feat: update bootstrap url

--- src/formats/javascript.ts
+++ src/formats/javascript.ts
@@ -13,7 +13,7 @@ import { ImportMap } from '../import_map.js'
 import type { FormatFunction } from '../server/server.js'
 import { getFileHash } from '../utils/sha256.js'
 
-const BOOTSTRAP_LATEST = 'https://62d144a15553b50009af7ac6--edge.netlify.com/bootstrap/index-combined.ts'
+const BOOTSTRAP_LATEST = 'https://62ea6d76a9595200092df21f--edge.netlify.com/bootstrap/index-combined.ts'
 
 interface BundleJSOptions {
   buildID: string
