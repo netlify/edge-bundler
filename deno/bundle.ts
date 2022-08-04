26c7e5e5794da2acc95b14349dcb615a8e19aceb
khen <30577427+khendrikse@users.noreply.github.com>
2022-08-04 10:22:52 +0200

    feat: change bootstrapurl to the deployId from edge.netlify.com

--- deno/bundle.ts
+++ deno/bundle.ts
@@ -1,4 +1,4 @@
-import { writeStage2 } from 'https://62ea6d76a9595200092df21f--edge.netlify.com/bundler/mod.ts'
+import { writeStage2 } from 'https://62ea8d05a6858300091547ed--edge.netlify.com/bundler/mod.ts'
 
 const [payload] = Deno.args
 const { basePath, destPath, functions, imports } = JSON.parse(payload)
