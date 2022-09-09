4ca97906ed02553c8157c01313c405451b1d10ea
Eduardo Bouças <mail@eduardoboucas.com>
2022-09-09 12:21:53 +0100

    feat: add support for import maps with ESZIP
    BREAKING CHANGE: `importMaps` now expects a `baseURL` containing the URL of the import map file

--- src/import_map.ts
+++ src/import_map.ts
@@ -2,24 +2,55 @@ import { Buffer } from 'buffer'
 import { promises as fs } from 'fs'
 import { dirname } from 'path'
 
+import { parse } from '@import-maps/resolve'
+
 const INTERNAL_IMPORTS = {
   'netlify:edge': 'https://edge.netlify.com/v1/index.ts',
 }
 
 interface ImportMapFile {
+  baseURL: URL
   imports: Record<string, string>
-  scopes?: Record<string, string>
+  scopes?: Record<string, Record<string, string>>
 }
 
 class ImportMap {
   imports: Record<string, string>
 
   constructor(input: ImportMapFile[] = []) {
-    const inputImports = input.reduce((acc, { imports }) => ({ ...acc, ...imports }), {})
+    const inputImports = input.reduce((acc, importMapFile) => {
+      const { imports } = ImportMap.resolve(importMapFile)
+
+      return {
+        ...acc,
+        ...imports,
+      }
+    }, {})
 
     // `INTERNAL_IMPORTS` must come last,
     // because we need to guarantee `netlify:edge` isn't user-defined.
     this.imports = { ...inputImports, ...INTERNAL_IMPORTS }
+  }
+
+  static resolve(importMapFile: ImportMapFile) {
+    const { baseURL, ...importMap } = importMapFile
+
+    // TODO: Add support for `scopes`.
+    const { imports } = parse(importMap, baseURL)
+
+    if (imports === undefined) {
+      return { imports: {} }
+    }
+
+    const resolvedImports: Record<string, string> = Object.entries(imports).reduce(
+      (acc, [key, value]) => ({
+        ...acc,
+        [key]: value instanceof URL ? value.toString() : value,
+      }),
+      {},
+    )
+
+    return { imports: resolvedImports }
   }
 
   getContents() {
