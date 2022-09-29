31ecfbf3a41ede7cf5c3f01c0225d97e1050c6d0
Eduardo Bouças <mail@eduardoboucas.com>
2022-09-29 10:55:36 +0100

    refactor: move config extractor to separate file

--- node/config.ts
+++ node/config.ts
@@ -1,5 +1,5 @@
-import { Buffer } from 'buffer'
 import { promises as fs } from 'fs'
+import { join } from 'path'
 import { pathToFileURL } from 'url'
 
 import tmp from 'tmp-promise'
@@ -7,6 +7,7 @@ import tmp from 'tmp-promise'
 import { DenoBridge } from './bridge.js'
 import { EdgeFunction } from './edge_function.js'
 import { Logger } from './logger.js'
+import { getPackagePath } from './package_json.js'
 
 // eslint-disable-next-line no-shadow
 enum ConfigExitCode {
@@ -23,89 +24,34 @@ export interface FunctionConfig {
   path?: string
 }
 
+const getConfigExtractor = () => {
+  const packagePath = getPackagePath()
+  const configExtractorPath = join(packagePath, 'deno', 'config.ts')
+
+  return configExtractorPath
+}
+
 export const getFunctionConfig = async (func: EdgeFunction, deno: DenoBridge, log: Logger) => {
   const collector = await tmp.file()
-  const script = `
-    const [ functionURL, collectorURL ] = Deno.args;
-
-    let func;
-
-    try {
-      func = await import(functionURL);
-    } catch {
-      Deno.exit(${ConfigExitCode.ImportError});
-    }
-
-    if (func.config === undefined) {
-      Deno.exit(${ConfigExitCode.NoConfig});
-    }
-
-    if (typeof func.config !== "function") {
-      Deno.exit(${ConfigExitCode.InvalidExport});
-    }
-
-    let config;
-
-    try {
-      config = await func.config();
-    } catch (error) {
-      console.error(error);
-
-      Deno.exit(${ConfigExitCode.RuntimeError});
-    }
-
-    try {
-      const result = JSON.stringify(config);
-
-      await Deno.writeTextFile(new URL(collectorURL), result);
-    } catch (err) {
-      Deno.exit(${ConfigExitCode.SerializationError});
-    }
-    
-    Deno.exit(${ConfigExitCode.Success});
-  `
-  const scriptURL = `data:application/javascript;base64,${Buffer.from(script).toString('base64')}`
+  const extractorPath = getConfigExtractor()
   const { exitCode } = await deno.run(
     [
       'run',
       '--allow-read',
       `--allow-write=${collector.path}`,
       '--quiet',
-      scriptURL,
+      extractorPath,
       pathToFileURL(func.path).href,
       pathToFileURL(collector.path).href,
+      JSON.stringify(ConfigExitCode),
     ],
     { pipeOutput: true, rejectOnExitCode: false },
   )
 
-  switch (exitCode) {
-    case ConfigExitCode.ImportError:
-      log.user(`Could not load edge function at '${func.path}'`)
+  if (exitCode !== ConfigExitCode.Success) {
+    logConfigError(func, exitCode, log)
 
-      return {}
-
-    case ConfigExitCode.NoConfig:
-      log.system(`No in-source config found for edge function at '${func.path}'`)
-
-      return {}
-
-    case ConfigExitCode.InvalidExport:
-      log.user(`'config' export in edge function at '${func.path}' must be a function`)
-
-      return {}
-
-    case ConfigExitCode.RuntimeError:
-      log.user(`Error while running 'config' function in edge function at '${func.path}'`)
-
-      return {}
-
-    case ConfigExitCode.SerializationError:
-      log.user(`'config' function in edge function at '${func.path}' must return an object with primitive values only`)
-
-      return {}
-
-    default:
-    // no-op
+    return {}
   }
 
   try {
@@ -113,10 +59,42 @@ export const getFunctionConfig = async (func: EdgeFunction, deno: DenoBridge, lo
 
     return JSON.parse(collectorData) as FunctionConfig
   } catch {
-    log.user(`Could not load configuration for edge function at '${func.path}'`)
+    logConfigError(func, ConfigExitCode.UnhandledError, log)
 
     return {}
   } finally {
     await collector.cleanup()
+  }
+}
+
+const logConfigError = (func: EdgeFunction, exitCode: number, log: Logger) => {
+  switch (exitCode) {
+    case ConfigExitCode.ImportError:
+      log.user(`Could not load edge function at '${func.path}'`)
+
+      break
+
+    case ConfigExitCode.NoConfig:
+      log.system(`No in-source config found for edge function at '${func.path}'`)
+
+      break
+
+    case ConfigExitCode.InvalidExport:
+      log.user(`'config' export in edge function at '${func.path}' must be a function`)
+
+      break
+
+    case ConfigExitCode.RuntimeError:
+      log.user(`Error while running 'config' function in edge function at '${func.path}'`)
+
+      break
+
+    case ConfigExitCode.SerializationError:
+      log.user(`'config' function in edge function at '${func.path}' must return an object with primitive values only`)
+
+      break
+
+    default:
+      log.user(`Could not load configuration for edge function at '${func.path}'`)
   }
 }
