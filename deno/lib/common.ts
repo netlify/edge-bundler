b084f01c91221397f2b13832f62a1152ff5e52a0
Eduardo Bouças <mail@eduardoboucas.com>
2022-09-30 12:18:02 +0100

    chore: reformat file

--- deno/lib/common.ts
+++ deno/lib/common.ts
@@ -1,40 +1,44 @@
-import { load } from 'https://deno.land/x/eszip@v0.28.0/loader.ts'
-import { LoadResponse } from 'https://deno.land/x/eszip@v0.28.0/mod.ts'
-import * as path from 'https://deno.land/std@0.127.0/path/mod.ts'
-import { retryAsync } from 'https://deno.land/x/retry@v2.0.0/mod.ts'
+import { load } from "https://deno.land/x/eszip@v0.28.0/loader.ts";
+import { LoadResponse } from "https://deno.land/x/eszip@v0.28.0/mod.ts";
+import * as path from "https://deno.land/std@0.127.0/path/mod.ts";
+import { retryAsync } from "https://deno.land/x/retry@v2.0.0/mod.ts";
 
 const inlineModule = (specifier: string, content: string): LoadResponse => {
   return {
     content,
     headers: {
-      'content-type': 'application/typescript',
+      "content-type": "application/typescript",
     },
-    kind: 'module',
+    kind: "module",
     specifier,
-  }
-}
+  };
+};
 
-const loadFromVirtualRoot = async (specifier: string, virtualRoot: string, basePath: string) => {
-  const basePathURL = path.toFileUrl(basePath).toString()
-  const filePath = specifier.replace(virtualRoot.slice(0, -1), basePathURL)
-  const file = await load(filePath)
+const loadFromVirtualRoot = async (
+  specifier: string,
+  virtualRoot: string,
+  basePath: string,
+) => {
+  const basePathURL = path.toFileUrl(basePath).toString();
+  const filePath = specifier.replace(virtualRoot.slice(0, -1), basePathURL);
+  const file = await load(filePath);
 
   if (file === undefined) {
-    throw new Error(`Could not find file: ${filePath}`)
+    throw new Error(`Could not find file: ${filePath}`);
   }
 
-  return { ...file, specifier }
-}
+  return { ...file, specifier };
+};
 
 const loadWithRetry = (specifier: string, delay = 1000, maxTry = 3) => {
-  if (!specifier.startsWith('https://')) {
-    return load(specifier)
+  if (!specifier.startsWith("https://")) {
+    return load(specifier);
   }
 
   return retryAsync(() => load(specifier), {
     delay,
     maxTry,
-  })
-}
+  });
+};
 
-export { inlineModule, loadFromVirtualRoot, loadWithRetry }
+export { inlineModule, loadFromVirtualRoot, loadWithRetry };
