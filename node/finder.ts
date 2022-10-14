3b4b5257c7082c05f7bd783756c4da23fb4f71dd
Matt Kane <m@mk.gg>
2022-10-14 13:23:58 +0100

    chore: include entrypoints in directories

--- node/finder.ts
+++ node/finder.ts
@@ -8,9 +8,16 @@ const ALLOWED_EXTENSIONS = new Set(['.js', '.jsx', '.ts', '.tsx'])
 
 const findFunctionInDirectory = async (directory: string): Promise<EdgeFunction | undefined> => {
   const name = basename(directory)
-  const candidatePaths = [`${name}.js`, `index.js`, `${name}.ts`, `index.ts`].map((filename) =>
-    join(directory, filename),
-  )
+  const candidatePaths = [
+    `${name}.js`,
+    `index.js`,
+    `${name}.ts`,
+    `index.ts`,
+    `${name}.jsx`,
+    `index.jsx`,
+    `${name}.tsx`,
+    `index.tsx`,
+  ].map((filename) => join(directory, filename))
 
   let functionPath
 
