c3169234f7fb878233957ffa40de8c5ba2944db6
Eduardo Bouças <mail@eduardoboucas.com>
2022-10-19 11:47:49 +0100

    fix: use import maps in config evaluation

--- test/fixtures/with_config/.netlify/edge-functions/framework-func2.ts
+++ test/fixtures/with_config/.netlify/edge-functions/framework-func2.ts
@@ -1,1 +1,5 @@
-export default async () => new Response('Hello from framework function 2')
+export default async () => {
+  const greeting = greet('framework function 1')
+
+  return new Response(greeting)
+}
