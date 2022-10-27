e5f9f28f7a87e2843b9ba1d66c7124af6df0372f
Eduardo Bouças <mail@eduardoboucas.com>
2022-10-27 14:36:49 +0100

    feat: read function config

--- node/server/server.test.ts
+++ node/server/server.test.ts
@@ -7,25 +7,38 @@ import { test, expect } from 'vitest'
 import { fixturesDir } from '../../test/util.js'
 import { serve } from '../index.js'
 
-test('bundler serving functionality', async () => {
+test('Starts a server and serves requests for edge functions', async () => {
   const port = await getPort()
   const server = await serve({
     port,
   })
+  const functionPath = join(fixturesDir, 'serve_test', 'echo_env.ts')
 
-  const { success } = await server(
-    [
-      {
-        name: 'echo_env',
-        path: join(fixturesDir, 'serve_test', 'echo_env.ts'),
-      },
-    ],
+  const functions = [
+    {
+      name: 'echo_env',
+      path: functionPath,
+    },
+  ]
+  const options = {
+    getFunctionsConfig: true,
+  }
+  const { functionsConfig, graph, success } = await server(
+    functions,
     {
       very_secret_secret: 'i love netlify',
     },
+    options,
   )
-
   expect(success).toBe(true)
+
+  expect(functionsConfig).toEqual([{ path: '/my-function' }])
+
+  const graphEntry = graph?.modules.some(
+    // @ts-expect-error TODO: Module graph is currently not typed
+    ({ kind, mediaType, local }) => kind === 'esm' && mediaType === 'TypeScript' && local === functionPath,
+  )
+  expect(graphEntry).toBe(true)
 
   const response = await fetch(`http://0.0.0.0:${port}/foo`, {
     headers: {
