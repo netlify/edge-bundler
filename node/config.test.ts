8c121cfa0d16c1b7a6d9e15f8253e0609a6baa1d
Jackie Macharia <jackline.macharia@netlify.com>
2022-11-16 15:30:01 +0300

    chore: add tests

--- node/config.test.ts
+++ node/config.test.ts
@@ -219,3 +219,77 @@ test('Loads function paths from the in-source `config` function', async () => {
 
   await fs.rmdir(tmpDir.path, { recursive: true })
 })
+
+test('`getFunctionConfig` validates the default export', async () => {
+  const { path: tmpDir } = await tmp.dir()
+  const deno = new DenoBridge({
+    cacheDirectory: tmpDir,
+  })
+
+  const functions = [
+    {
+      name: 'func1',
+      source: `
+        const func = () => new Response("Hello world!")
+        export default func
+      `,
+    },
+    {
+      name: 'func2',
+      source: `
+        export const func = () => new Response("Hello world!")
+      `,
+    },
+    {
+      name: 'func3',
+      source: `
+        export const func = new Response("Hello world!")
+      `,
+    },
+  ]
+
+  for (const func of functions) {
+    const logger = {
+      user: vi.fn().mockResolvedValue(null),
+      system: vi.fn().mockResolvedValue(null),
+    }
+    const path = join(tmpDir, `${func.name}.ts`)
+
+    const noDefaultExportErr = `No default export found in edge function at '${path}'`
+
+    const defaultExportNotFunctionErr = `Default export in edge function at '${path}' must be a function`
+
+    await fs.writeFile(path, func.source)
+
+    const config = await getFunctionConfig(
+      {
+        name: func.name,
+        path,
+      },
+      new ImportMap([importMapFile]),
+      deno,
+      logger,
+    )
+
+    switch (func.name) {
+      case 'func1': {
+        expect(config).not.throw()
+
+        break
+      }
+      case 'func2': {
+        expect(config).toThrowError(noDefaultExportErr)
+
+        break
+      }
+      case 'func3': {
+        expect(config).toThrowError(defaultExportNotFunctionErr)
+
+        break
+      }
+      // No default
+    }
+  }
+
+  await deleteAsync(tmpDir, { force: true })
+})
