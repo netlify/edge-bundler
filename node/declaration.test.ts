0b39add099c49d9b616959fdfc130b2d8d1b88e8
Jackie Macharia <jackline.macharia@netlify.com>
2022-11-30 19:18:04 +0300

    fix: fix linting

--- node/declaration.test.ts
+++ node/declaration.test.ts
@@ -1,4 +1,4 @@
-import { test, expect, vi } from 'vitest'
+import { test, expect } from 'vitest'
 
 import { FunctionConfig } from './config.js'
 import { getDeclarationsFromConfig } from './declaration.js'
