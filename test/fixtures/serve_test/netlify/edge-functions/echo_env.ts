74313804915fe342b29629df117f9ac948d36d85
Eduardo Bouças <mail@eduardoboucas.com>
2022-12-13 09:55:03 +0000

    chore: fix fixture

--- test/fixtures/serve_test/netlify/edge-functions/echo_env.ts
+++ test/fixtures/serve_test/netlify/edge-functions/echo_env.ts
@@ -2,7 +2,7 @@ import { Config } from 'https://edge.netlify.com'
 
 import { yell } from 'helper'
 
-export default async () => new Response(yell(Deno.env.get('very_secret_secret')))
+export default () => new Response(yell(Deno.env.get('very_secret_secret') ?? ''))
 
 export const config: Config = () => ({
   path: '/my-function',
