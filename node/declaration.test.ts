393bec84044ef2423802efe5c47b0fb081e47a7d
Simon Knott <info@simonknott.de>
2022-12-19 10:27:20 +0100

    test: toml-defined declarations are respected

--- node/declaration.test.ts
+++ node/declaration.test.ts
@@ -127,3 +127,17 @@ test('In-source config works if path property is an empty array with cache value
 
   expect(getDeclarationsFromConfig(tomlConfig, funcConfig, deployConfig)).toEqual(expectedDeclarations)
 })
+
+test('netlify.toml-defined excludepaths are respected', () => {
+  const tomlConfig = [{ function: 'geolocation', path: '/geo/*', excludePath: '/geo/exclude' }]
+
+  const funcConfig = {}
+
+  const expectedDeclarations = [
+    { function: 'geolocation', path: '/geo/*', excludePath: '/geo/exclude',  },
+  ]
+
+  const declarations = getDeclarationsFromConfig(tomlConfig, funcConfig, deployConfig)
+
+  expect(declarations).toEqual(expectedDeclarations)
+})
