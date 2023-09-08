c5a95717aadd6e23cc47a1ba59b8d51a52046a43
Eduardo Bouças <mail@eduardoboucas.com>
2023-09-08 12:31:39 +0100

    feat: show better error message

--- node/bundler.test.ts
+++ node/bundler.test.ts
@@ -125,7 +125,7 @@ test('Adds a custom error property to user errors during bundling', async () => 
   }
 })
 
-test('Prints a nice error message when user tries importing NPM module', async () => {
+test('Prints a nice error message when user tries importing an npm module and npm support is disabled', async () => {
   expect.assertions(2)
 
   const { basePath, cleanup, distPath } = await useFixture('imports_npm_module')
@@ -143,6 +143,33 @@ test('Prints a nice error message when user tries importing NPM module', async (
     expect(error).toBeInstanceOf(BundleError)
     expect((error as BundleError).message).toEqual(
       `It seems like you're trying to import an npm module. This is only supported via CDNs like esm.sh. Have you tried 'import mod from "https://esm.sh/parent-2"'?`,
+    )
+  } finally {
+    await cleanup()
+  }
+})
+
+test('Prints a nice error message when user tries importing an npm module and npm support is enabled', async () => {
+  expect.assertions(2)
+
+  const { basePath, cleanup, distPath } = await useFixture('imports_npm_module_scheme')
+  const sourceDirectory = join(basePath, 'functions')
+  const declarations: Declaration[] = [
+    {
+      function: 'func1',
+      path: '/func1',
+    },
+  ]
+
+  try {
+    await bundle([sourceDirectory], distPath, declarations, {
+      basePath,
+      featureFlags: { edge_functions_npm_modules: true },
+    })
+  } catch (error) {
+    expect(error).toBeInstanceOf(BundleError)
+    expect((error as BundleError).message).toEqual(
+      `There was an error when loading the 'p-retry' npm module. Support for npm modules in edge functions is an experimental feature. Refer to https://ntl.fyi/edge-functions-npm for more information.`,
     )
   } finally {
     await cleanup()
